a0e961f6af3f614b47da0284e65894473658a3d6
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-11 15:10:07 +0200

    base structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamShow from './components/streams/StreamShow';
+import StreamList from './components/streams/StreamList';
+import StreamDelete from './components/streams/StreamDelete';
+import Header from './components/Header';
 
 const App = () => {
   return (
-      <div>App</div>
+      <div className="ui container">
+        <BrowserRouter>
+          <Header />
+          <Route path='/' exact component={StreamList}/>
+          <Route path='/streams/new' component={StreamCreate}/>
+          <Route path='/streams/edit' component={StreamEdit}/>
+          <Route path='/streams/delete' component={StreamDelete}/>
+          <Route path='/streams/show' component={StreamShow}/>
+        </BrowserRouter>
+      </div>
   );
 };
 
