c1b1f9b31a8d7e30d12d9095ee65afd46674566e
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-11 13:26:19 +0200

    react router try

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>PageOne
-    <a href="/pagetwo">PageTwo</a>
-  </div>
+    return <div>PageOne</div>
 };
 
 const PageTwo = () => {
-  return <div>PageTwo
-    <a href="/">PageOne</a>
-  </div>
+    return <div>PageTwo</div>
+};
+
+const Navigation = () => {
+    return <div>
+        <a href="/">PageOne</a>
+        <br/>
+        <a href="/pagetwo">PageTwo</a>
+        <br/><br/>
+    </div>
 };
 
 const App = () => {
-  return (
-      <div>
-        <BrowserRouter>
-          <Route path='/' exact component={PageOne}/>
-          <Route path='/pagetwo' component={PageTwo}/>
-        </BrowserRouter>
-      </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Route path='/' component={Navigation}/>
+                <Route path='/' exact component={PageOne}/>
+                <Route path='/pagetwo' component={PageTwo}/>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
