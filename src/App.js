746954955437276540d3718d1515fdd669c96083
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-11 13:10:22 +0200

    react router try

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+  return <div>PageOne
+    <a href="/pagetwo">PageTwo</a>
+  </div>
+};
+
+const PageTwo = () => {
+  return <div>PageTwo
+    <a href="/">PageOne</a>
+  </div>
+};
+
 const App = () => {
   return (
-      <div>App</div>
+      <div>
+        <BrowserRouter>
+          <Route path='/' exact component={PageOne}/>
+          <Route path='/pagetwo' component={PageTwo}/>
+        </BrowserRouter>
+      </div>
   );
 };
 
