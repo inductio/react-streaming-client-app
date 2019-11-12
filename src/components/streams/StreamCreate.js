49e86ea9450ef38bdd851a23db4922e2f1ac1430
Oleksandr Shustrov <alex.shustrov@gmail.com>
2019-11-12 02:51:49 +0200

    redux form with validation for creating stream implemented

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>
-        StreamCreate
-    </div>
+class StreamCreate extends Component {
+    renderError = ({error, touched}) => {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                   <div className="header">{error}</div>
+                </div>
+            )
+        }
+    };
+
+    renderInput = ({input, label, meta}) => {
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+        return (
+            <div className={className}>
+                <label htmlFor="">{label}</label>
+                <input {...input} autoComplete="off"/>
+                {this.renderError(meta)}
+            </div>
+        );
+    };
+
+    onSubmit = (formValues) => {
+        //console.log({formValues})
+        //send request ot API server logic here to be implemented
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" type="text" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" type="text" component={this.renderInput} label="Enter Description"/>
+
+                <button className="ui button blue" type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'Enter title!';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'Enter Description!';
+    }
+
+    return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
