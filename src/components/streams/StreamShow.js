0df9ea6d0d8e4ecdf30fd0d8d2b5c12b858e3e2c
Oleksandr Shustrov <alex.shustrov@gmail.com>
2019-11-13 22:35:22 +0200

    Showing stream implemented

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamShow = () => {
-    return <div>
-        StreamShow
-    </div>
+class StreamShow extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.streamId);
+    }
+
+    render() {
+        const {title, description} = this.props.stream;
+        return (
+            <div className="content ui">
+                <h1>{title}</h1>
+                <h5 className="ui description">{description}</h5>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id],
+        streamId: ownProps.match.params.id
+    }
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, {
+    fetchStream
+})(StreamShow)
