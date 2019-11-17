66c13db1bdc10343b6f60f6bcc96b308bbd90f7a
Oleksandr_Shustrov <oleksandr_shustrov@epam.com>
2019-11-18 01:08:50 +0200

    show stream with video player implemented

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef()
+    }
     componentDidMount() {
         this.props.fetchStream(this.props.streamId);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.streamId}.flv`
+        });
+
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
         const {title, description} = this.props.stream;
         return (
             <div className="content ui">
+                <video ref={this.videoRef} style={{'width': '100%'}} controls={true}/>
                 <h1>{title}</h1>
                 <h5 className="ui description">{description}</h5>
             </div>
